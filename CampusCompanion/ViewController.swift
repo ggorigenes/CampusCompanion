51bb75efbaab3529d9e6e122802f426e0ad69ef6
ggorigenes <itsmedindin@gmail.com>
2026-09-11 11:27:04 +0800

    Fixed Auto Layout Constraints

--- CampusCompanion/ViewController.swift
+++ CampusCompanion/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func getStarted(_ sender: Any) {
+        subtitleLabel.text = "Let's get started!"
+    }
 }
 
