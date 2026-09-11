a7594b1103cda15f64b64c4d44c8b6ea3ad5cfec
ggorigenes <itsmedindin@gmail.com>
2026-09-11 10:35:27 +0800

    Initial Commit

---
+++ CampusCompanion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CampusCompanion
+//
+//  Created by Geraldine Origenes on 9/11/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
